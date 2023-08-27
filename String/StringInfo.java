8d476b1e3168c1446b682ca0f30ca935038f0f2a
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-08-27 20:48:43 +0530

    String Information

---
+++ String/StringInfo.java
@@ -0,0 +1,13 @@
+package strings;
+
+public class StringInfo {
+    public static void main(String[] args) {
+        char arr[]= {'a','b','c','d'};
+        String str1 = " Anisha";
+        String str2 = new String("Anisha");
+
+        System.out.println(arr);
+        System.out.println(str1);
+        System.out.println(str2);
+    }
+}
