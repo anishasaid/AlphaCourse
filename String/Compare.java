f553464d435e2e83dd6ab10a1e3bd74df9896338
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-08-27 20:53:08 +0530

    String Function - compare

---
+++ String/Compare.java
@@ -0,0 +1,31 @@
+package strings;
+
+import java.net.SocketTimeoutException;
+
+import javax.lang.model.util.ElementScanner14;
+
+public class Compare {
+    public static void main(String[] args) {
+        String str1 = "Tony";
+        String str2 = "Tony";
+        String str3 = new String("Tony");
+
+        // if(str1 == str2){
+        //     System.out.println("Strings are equal");
+        // }else{
+        //     System.out.println("Strings are not equal");
+        // }
+
+        // if(str1 == str3 ){
+        //     System.out.println("Strings are equal");
+        // }else{
+        //     System.out.println("Strings are not equal");
+        // }
+
+        if(str1.equals(str3)){
+            System.out.println("Strings are equal");
+        }else{
+            System.out.println("strings are not equal");
+        }
+    }
+}
