85199170a4eb35e84fb91651a4b67663cae5f248
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-08-31 15:55:29 +0530

    Binary OR operation

---
+++ Bit_Manipulation/BinaryOr.java
@@ -0,0 +1,12 @@
+package Bit_Manipulation;
+
+public class BinaryOr {
+    public static void main(String[] args) {
+        System.out.println(5 | 6);
+    }
+    // OR table 
+    //  0 | 0 => 0
+    //  0 | 1 => 1
+    //  1 | 0 => 1
+    //  1 | 1 => 1
+}
