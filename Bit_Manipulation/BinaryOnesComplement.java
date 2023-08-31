71f5b8cedf7138690961872ede3a8e9b765fccc8
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-08-31 16:05:35 +0530

    Calculate ONE'S  Complement

---
+++ Bit_Manipulation/BinaryOnesComplement.java
@@ -0,0 +1,14 @@
+package Bit_Manipulation;
+
+public class BinaryOnesComplement {
+    public static void main(String[] args) {
+        System.out.println(~5);
+    }
+    
+    // Binary Ones Complement is also called NOT Operator
+    // in this we have to calculate 2's complement
+    // least significant bit (LSB) => most right digit
+    // most significant bit (MSB) => most left digit 
+    //if MSB is 0 => number is +ve
+    // if MSB is 1 => number is -ve
+}
