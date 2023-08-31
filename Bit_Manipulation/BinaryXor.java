c573e7637260f4b75169d97427b5459a8324fd93
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-08-31 16:02:04 +0530

    Binary XOR operaion

---
+++ Bit_Manipulation/BinaryXor.java
@@ -0,0 +1,13 @@
+package Bit_Manipulation;
+
+public class BinaryXor {
+    public static void main(String[] args) {
+        System.out.println(5^6);
+    }
+
+    // XOR table 
+    // 0 ^ 0 => 0
+    // 0 ^ 1 => 1
+    // 1 ^ 0 => 1
+    // 1 ^ 1 => 0
+}
