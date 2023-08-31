cb5941a4354e9f91a813c99954095d05852894eb
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-08-31 16:04:29 +0530

    Binary  Left Shift

---
+++ Bit_Manipulation/BinaryLeftShift.java
@@ -0,0 +1,12 @@
+package Bit_Manipulation;
+
+public class BinaryLeftShift {
+    public static void main(String[] args) {
+        System.out.println(5<<2);
+    }
+
+    // formula of a << b
+    //  a<< b = a * 2^b
+    //example   5 << 2 = 5 * 2^2 
+    //                 =  5 * 4 => 20
+}
