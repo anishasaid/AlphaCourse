0d58e1fbf1f50bcf95cd649a83d895c31ea00073
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-08-31 16:03:50 +0530

    Binary Right Shift

---
+++ Bit_Manipulation/BinaryRightShift.java
@@ -0,0 +1,11 @@
+package Bit_Manipulation;
+
+public class BinaryRightShift {
+    public static void main(String[] args) {
+        System.out.println(6 >> 1 );
+    }
+
+    // formula of a >> b 
+    // a>>b = a/2^b
+    //example    6>>1 = 6/2^1
+}
