358b7ddbaf144c07daf5e4bdab9a319e20419448
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-09-10 15:29:21 +0530

    check if a given array is sorted or not

---
+++ Recursion_Basic's/Problem6.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+//check if a given array is sorted or not
+public class Problem6 {
+
+    public static boolean isSorted(int arr[], int i){
+
+        //base case
+        if(i == arr.length-1){
+            return true;
+        }
+        if(arr[i] > arr[i+1]){
+            return false;
+        }
+        return isSorted(arr, i+1);
+    
+    }
+    
+    public static void main(String[] args) {
+       
+        int arr []  = {1,5};
+        System.out.println(isSorted(arr, 0));
+    }
+}
