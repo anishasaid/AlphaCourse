2392cf16ad9d0804fd083accc0e7a383132279b2
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-09-18 12:41:42 +0530

    Find if any pair in a Sorted and Rotated ArrayList has a target sum.

---
+++ ArrayList/PairSum3.java
@@ -0,0 +1,47 @@
+package ArrayList;
+
+import java.util.ArrayList;
+// Find if any pair in a Sorted and Rotated ArrayList has a 
+// target sum.
+public class PairSum3 {
+    public static boolean pairSum(ArrayList<Integer>list, int target){
+        int bp = -1;
+        int n = list.size();
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i) > list.get(i+1)){  // breaking point 
+                bp = i;
+                break;
+            }
+        }
+        
+        int lp = bp+1;
+        int rp = bp ;
+
+        while (lp != rp){
+            //case 1 
+            if(list.get(lp) + list.get(rp) == target ){
+                return true;
+            }
+            //case 2 
+            if(list.get(lp)+ list.get(rp) < target){
+                lp = (lp+1) % n;
+            }else{
+                rp = (n+rp-1) % n;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        // 11,15,6,8,9,10 - sorted and roated 
+        list.add(11);
+        list.add(15);
+        list.add(6);
+        list.add(8);
+        list.add(9);
+        list.add(10);
+
+        int target = 16 ;
+        System.out.println(pairSum(list, target));
+    }
+}
