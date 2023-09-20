bd22ac9d2af0e484116f49a5b5b8f0b30792f1b1
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-09-20 10:32:03 +0530

    Create a linked list form scratch

---
+++ LinkedList(ONE)/IntroLinkedList.java
@@ -0,0 +1,19 @@
+package LinkedListONE;
+
+//create class of LinkedList
+public class IntroLinkedList {
+    
+    // this is the blueprint of single node
+    public static class Node{
+        int data ;
+        Node next ; // referance variable 
+
+        public Node(int data){
+            this.data = data ;
+            this.next  = null;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
