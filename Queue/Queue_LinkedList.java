711fadf65a714fe15193d98d6f727f967bf89f14
anishasaid <116268672+anishasaid@users.noreply.github.com>
2023-10-18 14:23:35 +0530

    Create Queue by using Linked List

---
+++ Queue/Queue_LinkedList.java
@@ -0,0 +1,70 @@
+package Queue;
+
+// Create Queue by using LinkedList
+public class Queue_LinkedList {
+
+    public static class Node{
+        int data ;
+        Node next ;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static class Queue{
+        static Node head = null;
+        static Node tail = null;
+        
+        public static boolean isEmpty(){
+            return head == null && tail == null;
+        }
+
+        //Add
+        public static void add(int data){
+            Node newNode = new Node(data);
+            if(head == null ){
+                head = tail = newNode;
+            }
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        // Remove
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("queue is empty");
+                return -1;
+            }
+            int front = head.data;
+
+            //single element 
+            if(tail == head){
+                tail = head = null;
+            }else{
+                head = head.next;
+            }
+            return front;
+        }
+
+        //Peek()
+        public static int peek(){
+            if(isEmpty()){
+                System.out.println("queue is empty");
+                return -1;
+            }
+            return head.data;
+        }
+    }
+    public static void main(String[] args) {
+        Queue q = new Queue();
+        q.add(1);;
+        q.add(2);
+        q.add(3);
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
